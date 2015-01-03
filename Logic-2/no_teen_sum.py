e89c8a7958f77f25c30c06171bcb19a78c7429be
liam-m <liam-m@users.noreply.github.com>
2015-01-03 15:05:59 +0000

    Shorten no_teen_sum

--- Logic-2/no_teen_sum.py
+++ Logic-2/no_teen_sum.py
@@ -1,8 +1,2 @@
-def fix_teen(n):
-  if n in range(13, 20) and n not in (15, 16):
-    return 0
-  else:
-    return n
-
-def no_teen_sum(a, b, c):
-  return fix_teen(a) + fix_teen(b) + fix_teen(c)
+fix_teen=lambda n:(n,0)[12<n<15or 16<n<20]
+no_teen_sum=lambda*a:sum(map(fix_teen,a))
