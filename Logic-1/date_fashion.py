33304cc1182661a6e71565232c685567309c4c19
liam-m <liam-m@users.noreply.github.com>
2015-01-11 15:20:35 +0000

    Shorten date_fashion

--- Logic-1/date_fashion.py
+++ Logic-1/date_fashion.py
@@ -1,7 +1,1 @@
-def date_fashion(you, date):
-  if you <= 2 or date <= 2:
-    return 0
-  elif you >= 8 or date >= 8:
-    return 2
-  else:
-    return 1
+date_fashion=lambda y,d:[[1,2][y>7or d>7],0][y<3or d<3]
